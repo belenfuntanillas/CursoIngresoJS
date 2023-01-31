9c280b04f04145526f492c3c77d6d8d9f80bb7a2
belenfuntanillas <belenfuntanillasmail.com>
2023-01-31 12:53:37 -0300

    modifique ejercicio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
-/*
+/* Funtanillas Belén
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,13 @@
 /*
+Funtanillas Belén Ejercicio 2 E/S
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombrePersona; //declaración de variable
+	let apellidoPersona
+	nombrePersona = prompt("¿Cuál es tu nombre?");
+	apellidoPersona = prompt("¿Cuál es tu apellido?")
+	alert("su nombre es " +nombrePersona + " su apellido es " +apellidoPersona);
 }
 
