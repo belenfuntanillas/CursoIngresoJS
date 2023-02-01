58a7ccf429b4e73f5bdf5cdb362d3bef9945c11a
belenfuntanillas <belenfuntanillasmail.com>
2023-02-01 09:12:04 -0300

    modifique ejercicio

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,11 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+	//nombreIngresado=txtIdNombre.value; ESTO FUNCIONA SOLO EN GOOGLE
+	//                 HTML  traeme el elemento del ID el valor
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+	alert(nombreIngresado);
 
 }
 
