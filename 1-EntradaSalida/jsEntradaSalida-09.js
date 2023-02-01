b5aba9b61de27cffcb7d51be23515d2c093018b5
belenfuntanillas <belenfuntanillasmail.com>
2023-02-01 19:30:37 -0300

    modifique ejercicio

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,23 @@
-/*
+/*Funtanillas Belén
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+let sueldo;
+let resultado;
+let aumento; 
 
-	alert("ok");
+
+sueldo= document.getElementById("txtIdSueldo").value;
+sueldo= parseInt(sueldo);
+
+aumento= (sueldo * 10/100);
+resultado= (sueldo + aumento);
+
+
+document.getElementById("txtIdResultado").value = resultado;
+
+
 }
