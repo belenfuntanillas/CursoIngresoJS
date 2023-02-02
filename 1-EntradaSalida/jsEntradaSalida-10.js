c89735cc8cee948435a90d33ea61a85398964a21
belenfuntanillas <belenfuntanillasmail.com>
2023-02-02 09:55:44 -0300

    modifique ejercicio 10 y 10 bis

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,19 +3,42 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
+/*
 function mostrarAumento()
 {
 	let importe;
-	let descuento; 
+	let porcentaje; 
 	let resultado;
 
 	importe= document.getElementById("txtIdImporte").value; 
 	importe= parseInt(importe);
 
-	descuento= (importe * 25/100);
+	porcentaje= importe * 25/100;
 
-	resultado= (importe - descuento);
+	resultado= importe - porcentaje;
 
 	document.getElementById("txtIdResultado").value= resultado;
 
+}*/
+
+//10 bis Funtanillas Belén 
+function mostrarAumento()
+{
+	let importe;
+	let descuentoIngresado;
+	let resultado;
+	let descuento;
+// ingreso 100
+	importe = document.getElementById("txtIdImporte").value; 
+	importe = parseInt(importe); 
+//25 descuento
+	descuentoIngresado = prompt("Ingresar un porcentaje de descuento");
+	descuentoIngresado = parseInt(descuentoIngresado);
+	
+//              100    *   0,25   = 25
+	descuento = importe * descuentoIngresado/100;
+   //             100     - 25
+	resultado = importe - descuento;
+
+	document.getElementById("txtIdResultado").value = resultado;
 }
