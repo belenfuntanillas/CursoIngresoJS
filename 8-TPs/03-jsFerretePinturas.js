18dd3d9dafca75954efe2134dce793bd6cc689de
belenfuntanillas <belenfuntanillasmail.com>
2023-02-02 20:33:21 -0300

    modifique trabajo

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,28 @@
-/*3.	Para el departamento de Pinturas:
+/*3.	Funtanillas Belén 
+Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
-	
+    let fahrenheit;
+    let centigrados;
+
+    fahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value); 
+    
+    centigrados = (fahrenheit - 32 ) * 5/9;
+
+    alert(fahrenheit + " Fahrenheit son " + centigrados + " Centígrados"); 
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let centigrados;
+    let fahrenheit;
+   //  
+
+   centigrados = parseFloat(document.getElementById("txtIdTemperatura").value);
+   fahrenheit = (centigrados * 9/5) + 32; 
+
+   alert(centigrados + " Centigrados son " + fahrenheit + " Fahrenheit"); 
 }
