f46655df5070f4c5e98a68cae9db388949fe5d54
belenfuntanillas <belenfuntanillasmail.com>
2023-02-08 09:32:29 -0300

    modifique ejercicio

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,21 @@
+//Funtanillas Belén 
+/* Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad; 
+	let estadoCivil; 
+	let mensaje; 
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value; 
 
-}//FIN DE LA FUNCIÓN
+	if (edad > 18 && estadoCivil == "Soltero") 
+	{
+		mensaje = "Es soltero y no es menor";
+
+		alert(mensaje);
+	}
+
+}
