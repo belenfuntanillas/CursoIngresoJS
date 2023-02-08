94a69a7fc65ed18f4b5b63522186f680e0c8512b
belenfuntanillas <belenfuntanillasmail.com>
2023-02-08 09:21:13 -0300

    modifique ejercicio

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,21 @@
+//Funtanillas Belén
+/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'*/
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	let estadoCivil;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value; 
+
+	if (edad < 18 && estadoCivil != "Soltero") 
+	{
+		mensaje = "Es muy pequeño para NO ser soltero";
+		
+		alert(mensaje); 
+
+	}
 	
-
-
-}//FIN DE LA FUNCIÓN
+}
