3ffdb5782c5bf73114fadabe55870f47b9a3a34c
belenfuntanillas <belenfuntanillasmail.com>
2023-02-08 10:23:19 -0300

    modifique ejercicio

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,29 @@
+//Funtanillas Belén
+/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4
+*/
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
+	let mensaje;
+
+	nota = Math.floor(Math.random() * 10) + 1;
+
+	if (nota >= 9) 
+	{
+		mensaje = "Excelente";
+	}else{
+		if (nota >= 4) 
+		{
+			mensaje = "Aprobó";
+		}else{
+			mensaje = "Vamos, la proxima se puede";
+		}
+	}
+
+	alert("Tu nota es un: " + nota   + ", "  + mensaje);
 
 }//FIN DE LA FUNCIÓN
