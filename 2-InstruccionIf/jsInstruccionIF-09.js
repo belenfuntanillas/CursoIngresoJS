4c311e8ff0ec816c7fc0cc0c4e4c1eb84b982200
belenfuntanillas <belenfuntanillasmail.com>
2023-02-08 10:08:22 -0300

    modifique ejercicio

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,11 @@
+//Funtanillas Belén
+/*Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive*/
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero;
 
-}//FIN DE LA FUNCIÓN
+	numero = Math.floor(Math.random() * 10) + 1;
+
+	alert(numero); 
+
+}
