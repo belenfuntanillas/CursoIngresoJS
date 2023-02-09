2ccf24d27ef3ac6f2a397cd6199edaef1b33f756
belenfuntanillas <belenfuntanillasmail.com>
2023-02-09 11:40:54 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,8 +1,35 @@
+//Funtanillas Belén
+/*al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mesDelAño; 
+	let mensaje; 
+
+	mesDelAño = document.getElementById("txtIdMes").value; 
+
+	switch (mesDelAño) 
+	{	
+		case "Agosto": 
+		case "Julio":
+			mensaje = "Abrigate que hace frío"; 
+			break;
+		case "Febrero":
+			mensaje = "Falta para el invierno"; 
+			break; 
+		case "Diciembre":
+			mensaje = "Ya paso el frio, ahora el calor!!!";
+			break; 
+		default :
+			mensaje = "Que tenga buen mes";
+	}
+
+	alert(mensaje);
 
 
 
