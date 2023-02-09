5f52242ac14db7c93ba1eeeb9e6c1cd3b0ad7906
belenfuntanillas <belenfuntanillasmail.com>
2023-02-09 12:12:10 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,30 @@
+//Funtanillas Belén
+/*Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.".*/
 function mostrar()
 {
 	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
+	let horaDelDia;
+	let mensaje; 
 
+	horaDelDia = parseInt(document.getElementById("txtIdHora").value); 
 
+	switch (horaDelDia) 
+	{
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			mensaje = "Es de mañana";
+			break;
+
+		default:
+			mensaje = "Que tenga buen día";
+			break;
+	}
+
+	alert(mensaje);
+	
 
 }//FIN DE LA FUNCIÓN
