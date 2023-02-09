aca19eb76f7ce78fb19e9faa0de2d06ec92f04dd
belenfuntanillas <belenfuntanillasmail.com>
2023-02-09 12:01:42 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,38 @@
+//Funtanillas Belén
+/*al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
+
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAño;
+	let mensaje; 
 
+	mesDelAño = document.getElementById("txtIdMes").value; 
 
+	switch (mesDelAño) 
+	{
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Septiembre":
+		case "Noviembre":
+		case "Enero":
+			mensaje = "Este mes tiene 31 días"; 
+			break;
+
+		case "Febrero":
+			mensaje = "Este mes tiene 28 días";
+			break;
+
+		default:
+			mensaje = "Este mes tiene 30 días";
+			break;
+	}
+
+	alert(mensaje);
+	
 
 }//FIN DE LA FUNCIÓN
