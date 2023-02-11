1551d5c8ba009e9a5b2802e65ad314202d1ebd72
belenfuntanillas <belenfuntanillasmail.com>
2023-02-11 15:10:12 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,33 @@
+//Funtanillas Belén
+/*Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste*/
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado;
+	let mensaje;
+
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+
+	switch (destinoIngresado) 
+	{
+		case "Bariloche":
+			mensaje = "Se encuentra al oeste del país";
+			break;
+	
+		case "Cataratas":
+			mensaje = "Se encuentra al norte del país";
+			break;
+
+		case "Mar del plata":
+			mensaje = "Se encuentra al este del país";
+			break; 
+
+		case "Ushuaia":
+			mensaje = "Se encuentra al sur del país";
+			break; 
+	}
+
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
