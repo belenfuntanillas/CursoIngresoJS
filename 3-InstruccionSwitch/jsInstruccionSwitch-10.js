b82d2bb6a865f939963cc0a421cc88a8d144837c
belenfuntanillas <belenfuntanillasmail.com>
2023-02-14 10:01:31 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,66 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionIngresada;
+	let destinoIngresado;
+	let mensaje;
+
+	estacionIngresada = document.getElementById("txtIdEstacion").value; 
+	destinoIngresado = document.getElementById("txtIdDestino").value; 
+
+	switch (estacionIngresada) 
+	{
+		case "Invierno":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+					mensaje = "Se viaja";
+					break;
+			
+				default:
+					mensaje = "No se viaja";
+					break;
+			}	
+			break;
+
+		case "Verano":
+			switch (destinoIngresado) 
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje = "Se viaja";
+					break;
+			
+				default: 
+				mensaje = "No se viaja";
+					break;
+			}
+			break; 
+
+		case "Otoño":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+				case "Mar del plata":
+				case "Cataratas":
+				case "Cordoba":
+					mensaje = "Se viaja";
+					break;
+			}
+		
+		case "Primavera":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+					mensaje = "No se viaja";
+					break;
+			
+				default:
+					mensaje = "Se viaja";
+					break;
+			}
+	}
+
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
