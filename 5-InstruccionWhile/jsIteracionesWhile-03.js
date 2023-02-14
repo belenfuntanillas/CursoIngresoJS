06a37380a1302ebc3a0a1fb5c443e1b4cbb005ef
belenfuntanillas <belenfuntanillasmail.com>
2023-02-14 20:18:23 -0300

    modifique ejercicio

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,15 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
+	let claveIngresada;
+	let clave; 
+
+	clave = "utn750"; 
+
+	while (claveIngresada != clave) 
+	{
+		claveIngresada = prompt("ingrese el número clave.");
+		
+	}
+
 }//FIN DE LA FUNCIÓN
