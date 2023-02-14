1660213fcbe2a126029a62603bda2efc3c423568
belenfuntanillas <belenfuntanillasmail.com>
2023-02-14 09:44:15 -0300

    modifique ejercicio

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,72 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionIngresada;
+	let destinoIngresado;
+	let precio;
+	let porcentaje;
+	let precioFinal;
+	let mensaje;
+
+	precio = 15000;
+	descuento = 0;
+
+	estacionIngresada = document.getElementById("txtIdEstacion").value; 
+	destinoIngresado = document.getElementById("txtIdDestino").value; 
+
+	switch (estacionIngresada) 
+	{
+		case "Invierno":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+					porcentaje = 20/100;
+					break;
+
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje = -10/100; 
+					break;
+
+				case "Mar del plata":
+					porcentaje = -20/100; 
+					break;
+			}
+			break;
+
+		case "Verano":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+					porcentaje = -20/100;
+					break;
+				
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje = 10/100; 
+					break;
+				
+				case "Mar del plata":
+					porcentaje = 20/100; 
+					break; 
+			}
+			break; 
+		
+		case "Otoño": 
+		case "Primavera":
+			switch (destinoIngresado) 
+			{
+				case "Bariloche":
+				case "Mar del plata":
+				case "Cataratas": 
+					porcentaje = 10/100;
+					break;
+			}
+	}
+	
+	precioFinal = precio * (precio + porcentaje);
+	mensaje = "El precio final del viaje es de $" + precioFinal; 
+
+	alert(mensaje); 
+
 
 }//FIN DE LA FUNCIÓN
