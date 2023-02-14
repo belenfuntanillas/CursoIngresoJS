eca3224ce3a658df2e301c953297656a026f04eb
belenfuntanillas <belenfuntanillasmail.com>
2023-02-14 20:26:33 -0300

    modifique ejercicio

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,16 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
+	let numeroIngresado;
+
+	numeroIngresado = parseInt(prompt("ingrese un número entre 0 y 9."));
+
+	while (isNaN(numeroIngresado) || numeroIngresado < -1 || numeroIngresado > 9)
+	{
+		numeroIngresado = parseInt(prompt("ERROR: Ingrese un número entre 0 y 9"));
+		
+	}
+
+	 document.getElementById("txtIdNumero").value = numeroIngresado; 
+
 }//FIN DE LA FUNCIÓN
