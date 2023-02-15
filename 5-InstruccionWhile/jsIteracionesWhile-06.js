6ebe42b956b63b4f0977d88114366359e490c570
belenfuntanillas <belenfuntanillasmail.com>
2023-02-15 10:21:49 -0300

    modifique ejercicio

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,35 @@
+/* Funtanillas Belén
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.
+*/
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let contador;
+	let acumulador;
+	let numeroIngresado;
+	let promedio; 
 
-	contador=0;
-	acumulador=0;
+	contador = 0;
+	acumulador = 0;
+	numeroIngresado = 0;
+
 	
+	for(i = 0;   i < 5; i++) 
+	{
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+		numeroIngresado = parseInt(prompt("ingresa un numero"));
+
+		while (isNaN(numeroIngresado)) 
+		{
+			numeroIngresado = parseInt(prompt("ERROR: ingresa un numero"));
+		}
+
+		contador = contador + 1;
+		acumulador = acumulador + numeroIngresado; 
+	}
+	
+	promedio =  acumulador / contador; 
+
+	document.getElementById("txtIdSuma").value = acumulador; 
+	document.getElementById("txtIdPromedio").value = promedio;
+
 }//FIN DE LA FUNCIÓN
