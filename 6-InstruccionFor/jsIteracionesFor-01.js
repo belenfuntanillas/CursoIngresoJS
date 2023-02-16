ad4fc925775cc3cac7f5629987815f74c0f9424c
belenfuntanillas <belenfuntanillasmail.com>
2023-02-16 11:55:32 -0300

    modifique ejercicio

--- 6-InstruccionFor/jsIteracionesFor-01.js
+++ 6-InstruccionFor/jsIteracionesFor-01.js
@@ -1,4 +1,13 @@
+/* Funtanillas Belén
+al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert("ok");
+	let i; 
+ 
+	//inicio variable; evaluo condicion; incremento o decremento
+	for( i = 1;            i < 11;                 i++)
+	{
+		console.log(i); 
+	}
+
 }
