b8bbe00dfac13428294e1755137148c597643826
belenfuntanillas <belenfuntanillasmail.com>
2023-02-21 13:53:35 -0300

    modifique ejercicio

--- 6-InstruccionFor/jsIteracionesFor-02.js
+++ 6-InstruccionFor/jsIteracionesFor-02.js
@@ -2,7 +2,7 @@
 al presionar el botón mostrar 10 repeticiones con números , desde el 10 al 1.*/
 function mostrar()
 {
-	for( i = 1; i < 11; i--)
+	for( i = 10; i > 0; i--)
 	{
 		document.write(i + "<br>"); 
 	}
