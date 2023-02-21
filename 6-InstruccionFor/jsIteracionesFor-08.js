b2793ac9fb61d182687e2cd6c7d8f475ce65dc6d
belenfuntanillas <belenfuntanillasmail.com>
2023-02-21 16:38:28 -0300

    modifique ejercicio

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,33 @@
+/* Funtanillas Belén
+al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
+*/
 function mostrar()
-{
+{	
+	let numeroIngresado; 
+	let contadorDivisores; 
 
+	contadorDivisores = 0; 
 
-	alert("ok");
+	numeroIngresado = parseInt(prompt("Escriba un numero"));
+	while(isNaN(numeroIngresado))
+	{
+		numeroIngresado = parseInt(prompt("ERROR Escriba un numero"));
+	}
+	
+	for( i = 2; i < numeroIngresado; i++)
+	{
+		if(numeroIngresado % i == 0)
+		{
+				contadorDivisores++; 
+		}
+	}
 
+	if(contadorDivisores != 0 || numeroIngresado <= 1)
+	{
+		alert(numeroIngresado + " no es primo"); 
+	}else 
+	{	
+		alert(numeroIngresado + " es primo"); 
+	}
+	
 }//FIN DE LA FUNCIÓN
