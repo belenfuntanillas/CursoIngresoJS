8c365461c867c45f98b5a2bd5753e1f28a7e1509
belenfuntanillas <belenfuntanillasmail.com>
2023-02-21 13:36:20 -0300

    modifique ejercicio

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,32 @@
+/* Funtanillas Belén 
+al presionar el botón pedir un número. mostrar los numeros divisores
+desde el 1 al número ingresado, y mostrar la cantidad de numeros divisores encontrados.
+*/
 function mostrar()
 {
-
-	alert("ok");
+	let repeticiones; 
+	let contadorDivisores; 
+ 
+	contadorDivisores = 0; 
+	
+	repeticiones = parseInt(prompt("Ponga numero de repeticiones"));
+	if(isNaN(repeticiones))
+	{
+		repeticiones = parseInt(prompt("ERROR Ponga numero de repeticiones"));
+	}
+	else
+	{
+		for( i = 1; i < repeticiones; i++)
+			{
+				if(repeticiones % i === 0)
+				{
+					console.log(i); 
+					contadorDivisores = contadorDivisores + 1; 
+				}
+			}
+	}
+	
+	console.log("La cantidad de numeros divisores es: " + contadorDivisores); 
 
 
 }//FIN DE LA FUNCIÓN
