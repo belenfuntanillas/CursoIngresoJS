9a03a4fbc9653f90e5de362a6b9062d3db63ce7d
belenfuntanillas <belenfuntanillasmail.com>
2023-02-23 08:38:35 -0300

    modifique ejercicio

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -24,6 +24,7 @@ function mostrar()
 	let acumuladorEdadHombreSoltero;
 	let contadorEdadHombreSoltero; 
 
+	respuesta = "si"; 
 	banderaTemperatura == true;
 	mayorTemperatura = 0; 
 	contadorAbuelosTemperaturaAlta = 0;
@@ -32,78 +33,82 @@ function mostrar()
 	acumuladorEdadHombreSoltero = 0; 
 	contadorEdadHombreSoltero = 0; 
 
-
-	nombre = prompt("Ingrese su nombre");
-	while(!isNaN(nombre))
+	while(respuesta == "si")
 	{
-		nombre = prompt("ERROR Ingrese su nombre");
-	}
-
-	edad = parseInt(prompt("Ingrese su edad"))
-	while(isNaN(edad) || edad < 1 || edad > 110)
-	{
-		edad = prompt("ERROR Ingrese su edad");
-	}
-
-	sexo = prompt("Ingrese f (femenino) o m (masculino)");
-	while(!isNaN(sexo) && sexo != "f" && sexo != "F" && sexo != "m" && sexo != "M")
-	{
-		sexo = prompt("ERROR Ingrese f (femenino) o m (masculino)");
-	}
-
-	estadoCivil = prompt("Ingrese su estado civil (soltero, casado o viudo)"); 
-	while(!isNaN(estadoCivil) && estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo" && estadoCivil != "Soltero" && estadoCivil != "Casado" && estadoCivil != "Viudo")
-	{
-		estadoCivil = prompt("ERROR Ingrese su estado civil (soltero, casado o viudo)"); 
-
-	}
-
-	temperaturaCorporal = parseFloat(prompt("Ingrese su temperatura"));
-	while(isNaN(temperaturaCorporal) || temperaturaCorporal < 30 || temperaturaCorporal > 50)
-	{
-		temperaturaCorporal = parseFloat(prompt("ERROR Ingrese su temperatura"));
-
-	}
-
-	//punto A
-	if(banderaTemperatura == true || temperaturaCorporal > mayorTemperatura)
-	{
-		temperaturaCorporal = mayorTemperatura; 
-		nombreMayorTemperatura = nombre; 
-		banderaTemperatura = false; 
-	}
-
-	//punto B
-	if( edad > 18 && estadoCivil == "viudo" || estadoCivil == "Viudo")
-	{
-		contadorViudos = contadorViudos + 1; 
-	}
-
-	//punto C
-	if(sexo == "m" || sexo == "M")
-	{
-		sexo = "masculino"; 
-		if(estadoCivil == "soltero" || estadoCivil == "Soltero" || estadoCivil == "viudo" || estadoCivil == "Viudo")
+		nombre = prompt("Ingrese su nombre");
+		while(!isNaN(nombre))
 		{
-			contadorHombresSolteroViudo = contadorHombresSolteroViudo + 1; 
-		}
-		if(estadoCivil == "soltero" || estadoCivil == "Soltero")
-		{
-			acumuladorEdadHombreSoltero = acumuladorEdadHombreSoltero + edad; 
-			contadorEdadHombreSoltero = contadorEdadHombreSoltero + 1; 
+			nombre = prompt("ERROR Ingrese su nombre");
 		}
 
-	}else 
-	{
-		sexo = "femenino";
-	}
+		edad = parseInt(prompt("Ingrese su edad"))
+		while(isNaN(edad) || edad < 1 || edad > 110)
+		{
+			edad = prompt("ERROR Ingrese su edad");
+		}
 
-	if(edad > 60 && temperaturaCorporal > 38)
-	{
-		contadorAbuelosTemperaturaAlta = contadorAbuelosTemperaturaAlta + 1;
-	}
+		sexo = prompt("Ingrese f (femenino) o m (masculino)");
+		while(!isNaN(sexo) && sexo != "f" && sexo != "F" && sexo != "m" && sexo != "M")
+		{
+			sexo = prompt("ERROR Ingrese f (femenino) o m (masculino)");
+		}
 
-	promedioEdad = acumuladorEdadHombreSoltero / contadorEdadHombreSoltero; 
+		estadoCivil = prompt("Ingrese su estado civil (soltero, casado o viudo)"); 
+		while(!isNaN(estadoCivil) && estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo" && estadoCivil != "Soltero" && estadoCivil != "Casado" && estadoCivil != "Viudo")
+		{
+			estadoCivil = prompt("ERROR Ingrese su estado civil (soltero, casado o viudo)"); 
+
+		}
+
+		temperaturaCorporal = parseFloat(prompt("Ingrese su temperatura"));
+		while(isNaN(temperaturaCorporal) || temperaturaCorporal < 30 || temperaturaCorporal > 50)
+		{
+			temperaturaCorporal = parseFloat(prompt("ERROR Ingrese su temperatura"));
+
+		}
+
+		respuesta = "¿Desea continuar? si / no"
+
+		//punto A
+		if(banderaTemperatura == true || temperaturaCorporal > mayorTemperatura)
+		{
+			temperaturaCorporal = mayorTemperatura; 
+			nombreMayorTemperatura = nombre; 
+			banderaTemperatura = false; 
+		}
+
+		//punto B
+		if( edad > 18 && estadoCivil == "viudo" || estadoCivil == "Viudo")
+		{
+			contadorViudos = contadorViudos + 1; 
+		}
+
+		//punto C
+		if(sexo == "m" || sexo == "M")
+		{
+			sexo = "masculino"; 
+			if(estadoCivil == "soltero" || estadoCivil == "Soltero" || estadoCivil == "viudo" || estadoCivil == "Viudo")
+			{
+				contadorHombresSolteroViudo = contadorHombresSolteroViudo + 1; 
+			}
+			if(estadoCivil == "soltero" || estadoCivil == "Soltero")
+			{
+				acumuladorEdadHombreSoltero = acumuladorEdadHombreSoltero + edad; 
+				contadorEdadHombreSoltero = contadorEdadHombreSoltero + 1; 
+			}
+
+		}else 
+		{
+			sexo = "femenino";
+		}
+
+		if(edad > 60 && temperaturaCorporal > 38)
+		{
+			contadorAbuelosTemperaturaAlta = contadorAbuelosTemperaturaAlta + 1;
+		}
+
+		promedioEdad = acumuladorEdadHombreSoltero / contadorEdadHombreSoltero; 
+	}
 
 	console.log("El nombre de la persona con mas temperatura es: " + nombreMayorTemperatura);
 	console.log(contadorViudos + " es la cantidad de mayores de edad viudos"); 
